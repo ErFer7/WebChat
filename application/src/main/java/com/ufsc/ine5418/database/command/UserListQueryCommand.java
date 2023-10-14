350004a449846c7dc174c99ad9321b4b1afa1842
sammycosta <arminetah@hotmail.com>
2023-10-14 06:31:18 -0300

    simply query creation

--- application/src/main/java/com/ufsc/ine5418/database/command/UserListQueryCommand.java
+++ application/src/main/java/com/ufsc/ine5418/database/command/UserListQueryCommand.java
@@ -4,21 +4,18 @@ import static com.ufsc.ine5418.database.model.QUser.user;
 
 import java.util.List;
 
-import com.querydsl.jpa.impl.JPAQuery;
 import com.ufsc.ine5418.database.EntityManagerProvider;
+import com.ufsc.ine5418.database.JPAQueryFactory;
 import com.ufsc.ine5418.database.model.User;
-
-import jakarta.persistence.EntityManager;
 
 public class UserListQueryCommand {
 
 	public List<User> execute() {
-		EntityManager entityManager = EntityManagerProvider.getEntityManager();
-		JPAQuery<User> query = new JPAQuery<>(entityManager);
-
-		List<User> users = query.select(user).from(user).fetch();
-
-		entityManager.close();
-		return users;
+		try (JPAQueryFactory queryFactory = new JPAQueryFactory(EntityManagerProvider.getEntityManager())) {
+			return queryFactory.createQuery()
+					.select(user)
+					.from(user)
+					.fetch();
+		}
 	}
 }
