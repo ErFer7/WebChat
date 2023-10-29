d9ae2250ec804b23cde6811bc8002ef8ef7e0cc3
sammycosta <arminetah@hotmail.com>
2023-10-29 19:56:56 -0300

    removing unnused method

--- application/src/main/java/com/ufsc/webchat/utils/retry/RetryException.java
+++ application/src/main/java/com/ufsc/webchat/utils/retry/RetryException.java
@@ -1,13 +1,11 @@
 package com.ufsc.webchat.utils.retry;
 
-//import static com.ufsc.webchat.server.ClientPacketProcessor.logger;
-
 import java.io.Serial;
 
 public class RetryException extends Exception {
-    @Serial private static final long serialVersionUID = -2912419945463621961L;
+	@Serial private static final long serialVersionUID = -2912419945463621961L;
 
-    public RetryException() {
+	public RetryException() {
 		super();
 	}
 
@@ -15,7 +13,4 @@ public class RetryException extends Exception {
 		super(message);
 	}
 
-	public void logError(String message) {
-		//        logger.error("Invalid payload"); //?
-	}
 }
