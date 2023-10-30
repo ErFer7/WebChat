190ad5a56836129e5d8635a30544007f1a9f32a2
Eric Fernandes Evaristo <ericfernandes7@gmail.com>
2023-10-29 23:37:34 -0300

    final

--- deploy/main.py
+++ deploy/main.py
@@ -6,6 +6,7 @@ Sistema de deploy.
 from os.path import join
 from source.deployer import Deployer
 
+print('[O DEPLOYER ESTÁ IMCOMPLETO]')
+
 deployer = Deployer(join('config', 'production.json'))
 deployer.run()
-
